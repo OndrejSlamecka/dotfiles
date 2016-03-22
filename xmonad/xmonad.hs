7db2aa600c112258ca25b330ea7d89d92d58f20b
Ondrej Slamecka <ondrej@slamecka.cz>
2016-03-22 21:20:07 +0200

    Small cleaning of the xmonad.hs file

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,12 +1,12 @@
 import XMonad
 import XMonad.Layout.ResizableTile
-import qualified Data.Map as M -- for adding keys
-import Data.List -- intercalate
+import qualified Data.Map as M (fromList)
+import Data.List (intercalate)
 
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
-import XMonad.Util.Run -- spawnPipe
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
 
 import Graphics.X11.ExtraTypes.XF86 -- multimedia keys
 
@@ -24,19 +24,19 @@ volume x = intercalate " && " [m_on, h_on, vol]
 toggleSound = "pactl set-sink-mute 0 toggle"
 
 -- own keys
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
+myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList
     [
     -- resizableTile, use mod-a and mod-z
-      ((modm,               xK_a), sendMessage MirrorShrink)
-    , ((modm,               xK_z), sendMessage MirrorExpand)
+      ((modm,                 xK_a), sendMessage MirrorShrink)
+    , ((modm,                 xK_z), sendMessage MirrorExpand)
 
     -- mod-b to toggle bars
-    , ((modm,               xK_b), sendMessage ToggleStruts)
+    , ((modm,                 xK_b), sendMessage ToggleStruts)
 
     -- audio
-    , ((0 , xF86XK_AudioLowerVolume), spawn $ volume Dec)
-    , ((0 , xF86XK_AudioRaiseVolume), spawn $ volume Inc)
-    , ((0 , xF86XK_AudioMute), spawn toggleSound)
+    , ((0, xF86XK_AudioLowerVolume), spawn $ volume Dec)
+    , ((0, xF86XK_AudioRaiseVolume), spawn $ volume Inc)
+    , ((0,        xF86XK_AudioMute), spawn toggleSound)
     ]
 
 
@@ -44,12 +44,12 @@ barPP :: PP
 barPP = def { ppCurrent = wrap "[" "]"
             , ppVisible = wrap "(" ")"
             , ppUrgent  = xmobarColor "red" "yellow"
-            , ppLayout  = (\x -> "")
+            , ppLayout  = const ""
             }
 
 main = do
     xmobar <- spawnPipe "pkill -f '^xmobar'; stack exec xmobar ~/.xmonad/xmobar.hs"
-    xmonad $ def{
+    xmonad $ def {
       terminal = "urxvt"
     , normalBorderColor  = "#6272a4" -- or more classical: "#cccccc"
     , focusedBorderColor = "#ff5555" -- "#D32F2F"
