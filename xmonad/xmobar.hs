cce1d5bf951e996a9449d7d2d3d36ae79c1e46ae
Ondrej Slamecka <ondrej@slamecka.cz>
2016-04-05 17:04:17 +0300

    Restructure xmonad config

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -1,25 +1,28 @@
 -- see also http://ixday.github.io/pop-up-with-xmobar.html
 -- $ echo -e "\uf027"   -- to print UTF char to be used by Font Awesome
 
-Config { font = "xft:Ubuntu:size=10:antialias=true"
-       , additionalFonts = ["xft:FontAwesome-11"]
-       , bgColor = "black"
-       , fgColor = "grey"
-       , alpha = 50
-       , position = BottomP 2 2
-       , textOffset = -1
-       , iconOffset = -1
-       , lowerOnStart = True
-       , pickBroadest = False
-       , persistent = False
-       , hideOnStart = False
-       , iconRoot = "."
-       , allDesktops = True
-       , overrideRedirect = True
-       , commands = [ Run Date "%H:%M" "date" 10
-                    , Run Com "/home/ondra/.xmonad/volume.sh" [] "volume" 10
-                    , Run StdinReader ]
-       , sepChar = "%"
-       , alignSep = "}{"
-       , template = "%StdinReader% }{ <fn=1>%volume%</fn>   %date%"
-       }
+Config
+  { font = "xft:Ubuntu:size=10:antialias=true"
+  , additionalFonts  = ["xft:FontAwesome-11"]
+  , bgColor          = "black"
+  , fgColor          = "grey"
+  , alpha            = 50
+  , position         = BottomP 2 2
+  , textOffset       = -1
+  , iconOffset       = -1
+  , lowerOnStart     = True
+  , pickBroadest     = False
+  , persistent       = False
+  , hideOnStart      = False
+  , iconRoot         = "."
+  , allDesktops      = True
+  , overrideRedirect = True
+  , sepChar          = "%"
+  , alignSep         = "}{"
+  , template = "%StdinReader% }{ <fn=1>%volume%</fn>   %date%"
+  , commands =
+    [ Run Date "%H:%M" "date" 10
+      , Run Com "/home/ondra/.xmonad/volume.sh" [] "volume" 10
+      , Run StdinReader
+    ]
+  }
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,61 +1,68 @@
+import qualified Data.Map (fromList)
 import XMonad
 import XMonad.Layout.ResizableTile
-import qualified Data.Map as M (fromList)
-import Data.List (intercalate)
-
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run (spawnPipe, hPutStrLn)
-
 import Graphics.X11.ExtraTypes.XF86 -- multimedia keys
 
--- volume settings
-data Vol = Inc | Dec
-instance Show Vol where
-    show Inc = "+"
-    show Dec = "-"
 
-volume x = intercalate " && " [m_on, h_on, vol]
-    where m_on = "amixer set Master on"
-          h_on = "amixer set Headphone on"
-          vol  = "amixer set Master 2" ++ show x
+-- Volume
+data Vol = Increase | Decrease | Toggle
 
-toggleSound = "pactl set-sink-mute 0 toggle"
-
--- own keys
-myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList
-    [
-    -- resizableTile, use mod-a and mod-z
-      ((modm,                 xK_a), sendMessage MirrorShrink)
-    , ((modm,                 xK_z), sendMessage MirrorExpand)
-
-    -- mod-b to toggle bars
-    , ((modm,                 xK_b), sendMessage ToggleStruts)
-
-    -- audio
-    , ((0, xF86XK_AudioLowerVolume), spawn $ volume Dec)
-    , ((0, xF86XK_AudioRaiseVolume), spawn $ volume Inc)
-    , ((0,        xF86XK_AudioMute), spawn toggleSound)
-    ]
+volume :: Vol -> X ()
+volume m = spawn $ case m of
+  Toggle   -> "amixer set Master toggle"
+  Increase -> on +&&+ modvol "+"
+  Decrease -> on +&&+ modvol "-"
+  where
+    modvol   = ("amixer set Master 2" ++)
+    on       = on_m +&&+ on_h
+    on_m     = "amixer set Master on"
+    on_h     = "amixer set Headphone on"
+    a +&&+ b = a ++ " && " ++ b
 
 
-barPP :: PP
-barPP = def { ppCurrent = wrap "[" "]"
-            , ppVisible = wrap "(" ")"
-            , ppUrgent  = xmobarColor "red" "yellow"
-            , ppLayout  = const ""
-            }
-
+-- XMonad
+main :: IO ()
 main = do
-    xmobar <- spawnPipe "pkill -f '^xmobar'; stack exec xmobar ~/.xmonad/xmobar.hs"
-    xmonad $ def {
-      terminal = "urxvt"
-    , normalBorderColor  = "#6272a4" -- or more classical: "#cccccc"
-    , focusedBorderColor = "#ff5555" -- "#D32F2F"
-    , layoutHook = avoidStruts $ smartBorders $ ResizableTall 1 (3/100) (1/2) []
-    , keys = myKeys <+> keys def
-    , logHook = dynamicLogWithPP barPP { ppOutput = hPutStrLn xmobar }
-    , modMask = mod4Mask -- use the Win key as mod
-    }
+  myXmobar <- spawnPipe "pkill -f '^xmobar'; stack exec xmobar ~/.xmonad/xmobar.hs"
+  xmonad $ def {
+    terminal           = "urxvt"
+  , normalBorderColor  = "#6272a4" -- the Dracula theme
+  , focusedBorderColor = "#ff5555"
+  , layoutHook         = myLayoutHook
+  , keys               = myKeys <+> keys def
+  , logHook            = myLogHook myXmobar
+  , modMask            = mod4Mask -- use the Win key as mod
+  , handleEventHook    = docksEventHook
+  }
+  where
+    myLayoutHook = avoidStruts . smartBorders $ (tall ||| wide)
+      where tall = ResizableTall 1 (3/100) (1/2) []
+            wide = Mirror tall
+
+    myKeys XConfig {XMonad.modMask = modm} = Data.Map.fromList
+      [
+      -- resizableTile, use mod-a and mod-z
+        ((modm,                 xK_a), sendMessage MirrorShrink)
+      , ((modm,                 xK_z), sendMessage MirrorExpand)
+
+      -- mod-b to toggle bars
+      , ((modm,                 xK_b), sendMessage ToggleStruts)
+
+      -- audio
+      , ((0, xF86XK_AudioLowerVolume), volume Decrease)
+      , ((0, xF86XK_AudioRaiseVolume), volume Increase)
+      , ((0,        xF86XK_AudioMute), volume Toggle)
+      ]
+
+    myLogHook bar = dynamicLogWithPP $ def
+      { ppCurrent = wrap "[" "]"
+      , ppVisible = wrap "(" ")"
+      , ppUrgent  = xmobarColor "red" "yellow"
+      , ppLayout  = const ""
+      , ppOutput  = hPutStrLn bar
+      }
 
