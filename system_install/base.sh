21e03cdb4ee0754659f59a495c5633db8c35d9c3
Ondrej Slamecka <ondrejslamecka@gambitresearch.com>
2018-07-06 16:21:41 +0100

    Add quotes around the argument to echo

--- system_install/base.sh
+++ system_install/base.sh
@@ -39,7 +39,7 @@ hwclock --systohc --utc
 
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 timedatectl set-ntp true # time sync daemon
 
