e3de0ec4a662cd901d5d381c31910b78bab963c6
Ondrej Slamecka <ondrejslamecka@gambitresearch.com>
2018-10-01 19:15:17 +0100

    Properly bind keys in fish
    https://fishshell.com/docs/current/commands.html#bind

--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,15 @@ set -gx EDITOR nvim
 
 function fish_user_key_bindings
 	fzf_key_bindings
+
+    # See functions directory for the following
+    bind \e\e sudope
+    bind . expand_dots
 end
 
 kitty + complete setup fish | source
 abbr kssh=kitty +kitten ssh
 
 # See functions directory for the following
-bind \e\e sudope
-bind . expand_dots
 install_git_abbr
 colorman
