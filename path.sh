c4732d4231861eaabe594b28e3fd36e33a688754
Ondrej Slamecka <ondrejslamecka@gambitresearch.com>
2018-11-11 08:58:59 +0000

    Change path of npm package dir

--- path.sh
+++ path.sh
@@ -4,8 +4,6 @@
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 
-NPM_PACKAGES="${HOME}/.npm-packages"
+NPM_PACKAGES="${HOME}/.local/npm-packages"
 
 export PATH="${PATH}:$NPM_PACKAGES/bin:$HOME/.local/bin"
-
-
