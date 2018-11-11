95497a11398207c7fed6ed3aa9b78451cbb7c74e
Ondrej Slamecka <ondrej@slamecka.cz>
2018-11-11 13:22:18 +0000

    I don't really use dropbox anymore

--- system_install/user.sh
+++ system_install/user.sh
@@ -26,7 +26,6 @@ pikaur --noconfirm --needed -S \
     unclutter-xfixes-git \
     powerline-fonts-git \
     google-chrome \
-    dropbox dropbox-cli \
     keybase-bin \
     browserpass \
     ttf-font-awesome \
